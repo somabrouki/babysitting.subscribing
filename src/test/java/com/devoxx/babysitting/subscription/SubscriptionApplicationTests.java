9d9d11e817290c7f6122dc9e93467da49e158a72
omabrouki <olfa.mabrouki@sylpheo.com>
2018-02-18 19:59:11 +0100

    Initialize the project

---
+++ src/test/java/com/devoxx/babysitting/subscription/SubscriptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devoxx.babysitting.subscription;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SubscriptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
